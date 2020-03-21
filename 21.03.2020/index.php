0acb422d437ddebbe998742fc1e511df12ee80b1
Viktor <46136749+toravb@users.noreply.github.com>
2020-03-21 16:07:05 +0200

    Update index.php

--- 21.03.2020/index.php
+++ 21.03.2020/index.php
@@ -13,6 +13,7 @@ if ($date != null){
 $currentMonth = date('m', strtotime($date));
 $nextMonth = $currentMonth + 1;
 
+//получаем дату, имя пользователя и ссылку по id из первой таблицы
 $data = $connect->prepare("SELECT user_id.date, link.author, link.link FROM user_id  LEFT JOIN link ON  user_id.id = link.id WHERE MONTH(`date`) = :currentMonth OR MONTH(`date`) = :nextMonth");
 $data->bindValue(':currentMonth', $currentMonth, PDO::PARAM_STR);
 $data->bindValue(':nextMonth', $nextMonth, PDO::PARAM_STR);
